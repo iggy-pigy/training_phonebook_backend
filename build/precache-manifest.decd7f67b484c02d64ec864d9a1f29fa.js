d10b6778373f0c8c0a9cc24ca959db132e6779d4
iggy-pigy <ilgakokorisa@gmail.com>
2020-06-02 17:15:53 +0100

    Updated added build repository and index.js

---
+++ build/precache-manifest.decd7f67b484c02d64ec864d9a1f29fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3f8fde63ad3fa177e1ea2cbe4e3d12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "375861c4a62f943da1cc",
+    "url": "/static/js/2.39dc633b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39dc633b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ac77c581b0d6012ddd",
+    "url": "/static/js/main.90b82a7d.chunk.js"
+  },
+  {
+    "revision": "058427dc81e78b8d63e7",
+    "url": "/static/js/runtime-main.4e2ef531.js"
+  }
+]);
