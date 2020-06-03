ef182d936c57383ffa63d8625aeeb5937894d946
iggy-pigy <ilgakokorisa@gmail.com>
2020-06-03 15:32:47 +0100

    Added build

---
+++ build/precache-manifest.a0f594c0fdcd5a2c7166ae7af8fb13c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f86f3c1f428fd16006c13ed2c9bba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "375861c4a62f943da1cc",
+    "url": "/static/js/2.39dc633b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39dc633b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42d1289d75449817f67",
+    "url": "/static/js/main.4ad21b8e.chunk.js"
+  },
+  {
+    "revision": "058427dc81e78b8d63e7",
+    "url": "/static/js/runtime-main.4e2ef531.js"
+  }
+]);
