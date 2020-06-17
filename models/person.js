8c8c9e9cb6b2ffda640ac20d630f3c87328c4c3a
iggy-pigy <ilgakokorisa@gmail.com>
2020-06-17 22:02:48 +0100

    Updated personSchema

--- models/person.js
+++ models/person.js
@@ -17,7 +17,7 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const personSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true, minlength: 3 },
-    number: { type: Number, required: true, minlength: 5 },
+    number: { type: String, required: true, minlength: 8 },
 })
 
 personSchema.set('toJSON', {
